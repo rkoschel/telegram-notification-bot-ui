a25e3f2656c68e5bafa0d4b2a305c435ff8b26d4
rkoschel <rudolf@koschel.org>
2023-10-20 10:37:46 +0200

    initial material designed angular app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gwfjt-ui'`, () => {
+  it(`should have as title 'tnb-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gwfjt-ui');
+    expect(app.title).toEqual('tnb-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gwfjt-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tnb-ui app is running!');
   });
 });
